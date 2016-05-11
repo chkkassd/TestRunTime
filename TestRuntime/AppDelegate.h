ae1e2b7a33b45b0425d27e98058d6004169e91eb
peter.shi <532289386@qq.com>
2016-05-11 14:48:57 +0800

    Initial Commit

---
+++ TestRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRuntime
+//
+//  Created by 赛峰 施 on 16/5/11.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRuntime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestRuntime
+//
+//  Created by 赛峰 施 on 16/5/11.
+//  Copyright © 2016年 赛峰 施. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
